7b22a9953049ec3b963d20d27b43e705ea9e5784
learnuidev <learnuidev@gmail.com>
2024-06-05 22:04:25 -0400

    added styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,310 @@
 import Image from "next/image";
 
+const words = [
+  {
+    lang: "en",
+    input: "no",
+  },
+  {
+    lang: "en",
+    input: "yes",
+  },
+  {
+    lang: "en",
+    input: "hello",
+  },
+  {
+    lang: "en",
+    input: "bye",
+  },
+  {
+    lang: "en",
+    input: "I",
+  },
+  {
+    lang: "en",
+    input: "I am",
+  },
+  {
+    lang: "en",
+    input: "you are",
+  },
+  {
+    lang: "en",
+    input: "what",
+  },
+  {
+    lang: "en",
+    input: "is the",
+  },
+  {
+    lang: "en",
+    input: "can",
+  },
+  {
+    lang: "en",
+    input: "this",
+  },
+  {
+    lang: "en",
+    input: "to be",
+  },
+  {
+    lang: "en",
+    input: "but",
+  },
+  {
+    lang: "en",
+    input: "a",
+  },
+  {
+    lang: "en",
+    input: "it",
+  },
+  {
+    lang: "en",
+    input: "to",
+  },
+  {
+    lang: "en",
+    input: "of",
+  },
+  {
+    lang: "en",
+    input: "or",
+  },
+  {
+    lang: "en",
+    input: "if",
+  },
+  {
+    lang: "en",
+    input: "with",
+  },
+  {
+    lang: "en",
+    input: "these",
+  },
+  {
+    lang: "en",
+    input: "where",
+  },
+  {
+    lang: "en",
+    input: "why",
+  },
+  {
+    lang: "en",
+    input: "how",
+  },
+  {
+    lang: "en",
+    input: "to do",
+  },
+  {
+    lang: "en",
+    input: "there",
+  },
+  {
+    lang: "en",
+    input: "as",
+  },
+  {
+    lang: "en",
+    input: "his",
+  },
+  {
+    lang: "en",
+    input: "that",
+  },
+  {
+    lang: "en",
+    input: "he",
+  },
+  {
+    lang: "en",
+    input: "on",
+  },
+  {
+    lang: "en",
+    input: "they",
+  },
+  {
+    lang: "en",
+    input: "at",
+  },
+  {
+    lang: "en",
+    input: "to have",
+  },
+  {
+    lang: "en",
+    input: "some",
+  },
+  {
+    lang: "en",
+    input: "you all",
+  },
+  {
+    lang: "en",
+    input: "had",
+  },
+  {
+    lang: "en",
+    input: "and",
+  },
+  {
+    lang: "en",
+    input: "in",
+  },
+  {
+    lang: "en",
+    input: "we",
+  },
+  {
+    lang: "en",
+    input: "were",
+  },
+  {
+    lang: "en",
+    input: "which",
+  },
+  {
+    lang: "en",
+    input: "other",
+  },
+  {
+    lang: "en",
+    input: "time",
+  },
+  {
+    lang: "en",
+    input: "will",
+  },
+  {
+    lang: "en",
+    input: "said",
+  },
+  {
+    lang: "en",
+    input: "each",
+  },
+  {
+    lang: "en",
+    input: "to want",
+  },
+  {
+    lang: "en",
+    input: "good",
+  },
+  {
+    lang: "en",
+    input: "bad",
+  },
+  {
+    lang: "en",
+    input: "also",
+  },
+  {
+    lang: "en",
+    input: "again",
+  },
+  {
+    lang: "en",
+    input: "can you how many",
+  },
+  {
+    lang: "en",
+    input: "to come",
+  },
+  {
+    lang: "en",
+    input: "to know",
+  },
+  {
+    lang: "en",
+    input: "sure",
+  },
+  {
+    lang: "en",
+    input: "both",
+  },
+  {
+    lang: "en",
+    input: "to forget",
+  },
+  {
+    lang: "en",
+    input: "half",
+  },
+  {
+    lang: "en",
+    input: "until",
+  },
+  {
+    lang: "en",
+    input: "except",
+  },
+  {
+    lang: "en",
+    input: "definitely",
+  },
+  {
+    lang: "en",
+    input: "Sometimes",
+  },
+  {
+    lang: "en",
+    input: "wait",
+  },
+  {
+    lang: "en",
+    input: "here",
+  },
+  {
+    lang: "en",
+    input: "people",
+  },
+  {
+    lang: "en",
+    input: "Something",
+  },
+  {
+    lang: "en",
+    input: "now",
+  },
+  {
+    lang: "en",
+    input: "before",
+  },
+  {
+    lang: "en",
+    input: "after day",
+  },
+  {
+    lang: "en",
+    input: "week",
+  },
+  {
+    lang: "en",
+    input: "month to eat",
+  },
+  {
+    lang: "en",
+    input: "all",
+  },
+];
+
 export default function Home() {
-  const words = `no, yes, hello, bye, I, I am, you are, what, is the, can, this, to be, but, a, it, to, of, or, if, with, these, where, why, how, to do, there, as, his, that, he, on, they, at, to have, some, you all, had, and, in, we, were, which, other, time, will, said, each, to want, good, bad, also, again, can you how many, to come, to know, sure, both, to forget, half, until, except, definitely, Sometimes, wait, here, people, Something, now, before, after day, week, month to eat, all`;
-  return <main> {words}</main>;
+  return (
+    <main className="grid gap-8 sm:grid-cols-2 lg:grid-cols-4 md:grid-cols-3 p-4 mt-32">
+      {words?.map((word) => {
+        return (
+          <button className="text-gray-400 text-2xl" key={word?.input}>
+            {word?.input}
+          </button>
+        );
+      })}
+    </main>
+  );
 }
